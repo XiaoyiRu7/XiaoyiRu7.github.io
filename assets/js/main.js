d1e3ec216516390bcc9f81b6be31e2f56194d0a4
Xiaoyi Ru <tersa7ru@hotmail.com>
2018-11-02 19:13:49 +1100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,48 @@ $('.nav').click(function() {
   
 
 });
+
+
+$(document).ready(function() {
+  // Add jQuery here
+
+  $(".oase").mouseenter(function() {
+    $(".oase__image").addClass("showme");
+  });
+
+  $(".oase").mouseleave(function() {
+    $(".oase__image").removeClass("showme");
+  });
+
+  $(".magazine").mouseenter(function() {
+    $(".magazine__image").addClass("showme");
+  });
+
+  $(".magazine").mouseleave(function() {
+    $(".magazine__image").removeClass("showme");
+  });
+
+  $(".landscape").mouseenter(function() {
+    $(".landscape__image").addClass("showme");
+  });
+
+  $(".landscape").mouseleave(function() {
+    $(".landscape__image").removeClass("showme");
+  });
+  $(".step4").mouseenter(function() {
+    $(".step4__image").addClass("showme");
+  });
+
+  $(".step4").mouseleave(function() {
+    $(".step4__image").removeClass("showme");
+  });
+
+
+
+
+});
+
+ $( function() {
+    $( ".box" ).draggable();
+  } );
+
